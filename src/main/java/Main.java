8452b39cd6bdd9e0219fd2295d0d81a244728968
Daniil Bondarenko <dan_2015@mail.ru>
2018-01-28 20:40:35 +0300

    Added static pages: index.html, sign_in.html, sign_up.html.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,14 @@
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        ResourceHandler resource_handler = new ResourceHandler();
+        resource_handler.setResourceBase("src/main/resources/html");
+        resource_handler.setWelcomeFiles(new String[] {"index.html"});
+        Server server = new Server(8080);
+        server.setHandler(resource_handler);
+        server.start();
+        server.join();
     }
 }
